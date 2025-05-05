39df7fb1ec8dcf225c02fc692d66f696cb8ed3bb
Neha Methani <nehamethani54@gmail.com>
2025-05-05 15:28:24 +0530

    Added Controller

---
+++ src/main/java/com/nekocodes/JobApp/JobController.java
@@ -0,0 +1,16 @@
+package com.nekocodes.JobApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @GetMapping("/")
+    public String home()
+    {
+        System.out.println("Inside Home controller");
+        return "home";
+    }
+}
